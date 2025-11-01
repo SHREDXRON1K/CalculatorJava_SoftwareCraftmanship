043591313f4f658b1cfa125e9fa1cd43b1f28f61
SHREDXRON1K <94455223+SHREDXRON1K@users.noreply.github.com>
2025-11-01 11:23:51 +0100

    Add files via upload
    Added variable functionality, reduce and clean the code!

--- Calculator.java
+++ Calculator.java
@@ -1,45 +1,79 @@
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Calculator{
 
+private List< Map.Entry<String, Double> > variables_valuePairs = new ArrayList<>();
 
-    public List<Token> createTokenList(String expression) {
-        List <Token> toReturn = new ArrayList<>();
 
-        String expression_element;
+    public List<Token> createTokenList(String expression) throws Exception {
+    List<Token> toReturn = new ArrayList<>();
 
-        StringBuffer number_buffer = new StringBuffer();
+    String expression_element;
+    StringBuilder number_buffer = new StringBuilder();
+    StringBuilder variable_buffer = new StringBuilder();
+    String number;
+    String variable;
+    char expressionPosition;
+    Map.Entry<String, Double> pairsPosition;
+    boolean found = false;
 
-        String number;
+    for (int i = 0; i < expression.length(); i++) {
+        expressionPosition = expression.charAt(i);
 
-        for(int i=0; i<expression.length(); i++){
-            if (expression.charAt(i) >= '0' && expression.charAt(i) <= '9' || expression.charAt(i) == '.') {
-            number_buffer.append(expression.charAt(i));
-
-            }
-            else{
-                if(number_buffer.length() > 0){
-                number = number_buffer.toString();
-                toReturn.add(new Token(number, -1, Double.parseDouble(number)));
-                number_buffer.setLength(0);
-                }
-
-                expression_element = String.valueOf(expression.charAt(i));
-
-                if (expression_element.equals("+")) toReturn.add(new Token(expression_element, 1, 0));
-                else if (expression_element.equals("-")) toReturn.add(new Token(expression_element, 1, 0));
-                else if (expression_element.equals("*")) toReturn.add(new Token(expression_element, 2, 0));
-                else if (expression_element.equals("/")) toReturn.add(new Token(expression_element, 2, 0));
-                else if (expression_element.equals("(")) toReturn.add(new Token(expression_element, 0, 0));
-                else if (expression_element.equals(")")) toReturn.add(new Token(expression_element, 0, 0));
-            }
+        if ((expressionPosition >= '0' && expressionPosition <= '9') || expressionPosition == '.') {
+            number_buffer.append(expressionPosition);
+            continue;
         }
 
-        if (number_buffer.length() > 0) {
+        if ((expressionPosition >= 'a' && expressionPosition <= 'z')
+                || (expressionPosition >= 'A' && expressionPosition <= 'Z')) {
+            variable_buffer.append(expressionPosition);
+            continue;
+            // if (variables_valuePairs.isEmpty()) throw new Exception("Variable found but none defined: " + variable);
+            }
+        if(variable_buffer.length()>0){
+            variable = variable_buffer.toString();
+            for (int j = 0; j < variables_valuePairs.size(); j++) {
+                pairsPosition = variables_valuePairs.get(j);
+                if (variable.equals(pairsPosition.getKey())) {
+                    toReturn.add(new Token(variable, -1, pairsPosition.getValue()));
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) throw new Exception("Variable is not defined: " + variable);
+
+        }
+
+        
+        found = false;
+        variable_buffer.setLength(0);
+
+        
+        if(number_buffer.length() > 0){
         number = number_buffer.toString();
         toReturn.add(new Token(number, -1, Double.parseDouble(number)));
+        number_buffer.setLength(0);
         }
+
+        expression_element = String.valueOf(expressionPosition);
+
+            if (expression_element.equals("+")) toReturn.add(new Token(expression_element, 1, 0));
+            else if (expression_element.equals("-")) toReturn.add(new Token(expression_element, 1, 0));
+            else if (expression_element.equals("*")) toReturn.add(new Token(expression_element, 2, 0));
+            else if (expression_element.equals("/")) toReturn.add(new Token(expression_element, 2, 0));
+            else if (expression_element.equals("(")) toReturn.add(new Token(expression_element, 0, 0));
+            else if (expression_element.equals(")")) toReturn.add(new Token(expression_element, 0, 0));
+        }
+    
+
+    if (number_buffer.length() > 0) {
+    number = number_buffer.toString();
+    toReturn.add(new Token(number, -1, Double.parseDouble(number)));
+    }
 
         return toReturn;
     }
@@ -140,45 +174,71 @@ public class Calculator{
         return expression;   
     }
 
-    public Token applyRules(List<Token> expression){
+    public Token applyRules(List<Token> expression) {
 
-        int degree;
+    int degree;
+    Token operator;
+    Token operand1;
+    Token operand2;
 
-        Token operator;
-        Token operand1;
-        Token operand2;
-        int i;
+    while (expression.size() != 1) {
 
-        while(expression.size()!=1){
+        degree = checkHighestDegree(expression);
 
-            degree = checkHighestDegree(expression);
+        for (int i = 0; i < expression.size(); i++) {
+            if (expression.get(i).getSymbol().equals("(")) {
+                expression = handleBracket(i, expression);
+                i = -1; // restart after modification
+                continue;
+            }
 
-            for(i=0; i<expression.size(); i++){
-                if(expression.get(i).getSymbol().equals("(") ){
-                    expression = handleBracket(i, expression);
+            operator = expression.get(i);
+
+            if (operator.isOperator()) {
+
+                // ✅ Fix: skip only invalid operator positions
+                if (i <= 0 || i >= expression.size() - 1) {
                     continue;
                 }
-                
-                operator = expression.get(i);
-                if(operator.isOperator()){
-                    operand1 = expression.get(i-1);
-                    operand2 = expression.get(i+1);
-                    if(!operand1.isOperator() && !operand2.isOperator()){
-                        if(operator.strength()==2 && degree==2) expression = Degree2Operation(i, expression);
-                        else if(operator.strength()==1 && degree==1) expression = Degree1Operation(i, expression); 
+
+                operand1 = expression.get(i - 1);
+                operand2 = expression.get(i + 1);
+
+                if (!operand1.isOperator() && !operand2.isOperator()) {
+                    if (operator.strength() == 2 && degree == 2) {
+                        expression = Degree2Operation(i, expression);
+                        i = -1; // restart after list change
+                    } else if (operator.strength() == 1 && degree == 1) {
+                        expression = Degree1Operation(i, expression);
+                        i = -1;
                     }
-                }               
+                }
             }
         }
-        return expression.get(0);
+    }
 
+    return expression.get(0);
+}
+
+
+    //Erweiterung fuer 1. Software Craftmanship Praktikum
+    
+    public void define(String variable, double value){
+        Map.Entry<String, Double> variable_value = new AbstractMap.SimpleEntry<>(variable, value);
+        variables_valuePairs.add(variable_value);
     }
 
     public double Calculate(String expression){
-        List<Token> term = createTokenList(expression);
-        Token end;
-        end = applyRules(term);
-        return end.getValue();
 
+        try{ 
+            List<Token> term = createTokenList(expression);
+            Token end;
+            end = applyRules(term);
+            return end.getValue();
+        }
+        catch(Exception e){
+            System.out.println("Exception : " + e.getMessage());
+        }
+        return Double.NaN;
     }
 }
--- main.java
+++ main.java
@@ -1,7 +1,8 @@
 public class main {
     public static void main(String[] args){
         Calculator a = new Calculator();
-        System.out.println( a.Calculate("22/7+2") );
+        a.define("pi",3.14);
+        System.out.println( a.Calculate("10+pi+10") );
     }
 }
 
